7fa6759b97cd825e48e0c4a46f432d193f0b4a23
PETEroid <selllfocard@gmail.com>
2016-08-02 01:27:51 -0700

    fix npm package

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 var Webpack = require('webpack')
+var _extend = require('util')._extend
 
-module.exports = {
+var commonConfig = {
   devtool: 'eval',
-  entry: [
-    './src/index.js'
-  ],
   module: {
     loaders: [
       {
@@ -15,8 +13,35 @@ module.exports = {
     ]
   },
   output: {
-    filename: "app.js",
-    path: __dirname + '/build/'
-  },
-  plugins: []
+    path: __dirname,
+    filename: "[name].js"
+  }
 }
+
+var dist = {
+  devtool: 'eval',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  entry: {'dist/Pentagon': './src/components/Pentagon.js'},
+  output: {
+    libraryTarget: "umd",
+    library: "Pentagon",
+    path: __dirname,
+    filename: "[name].js"
+  },
+  externals: {
+    react: "react"
+  }
+}
+
+var app = _extend({entry: {'build/app': './src/index.js'}}, commonConfig)
+
+module.exports = [dist, app]
+
